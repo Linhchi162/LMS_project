07f47ba5a7036c252f6de156ff617af54eeed8bc
TrMiDuc <124168615+TrMiDuc@users.noreply.github.com>
2024-03-22 19:51:30 +0900

    test_function

---
+++ php/function.php
@@ -0,0 +1,36 @@
+<?php
+    //get data of borrowed books
+    function get_borrowed($user_id) {
+        //connect to database
+        include 'connect.php';
+    
+        $query = "SELECT *
+        
+        FROM `account`
+        INNER JOIN `borrow` ON `borrow`.account_id = `account`.id
+        INNER JOIN `book` ON `borrow`.book_id = `book`.id
+        INNER JOIN `book_detail` ON `book`.id = `book_detail`.id
+        WHERE `book`.id = $user_id";
+    
+        $result = $mysqli->query($query);
+        $mysqli->close();
+        return $result;
+    }
+
+    //get data of borrowing books
+    function get_borrowing($user_id) {
+        //connect to database
+        include 'connect.php';
+    
+        $query = "SELECT * 
+        FROM `account`
+        INNER JOIN `borrow` ON `borrow`.account_id = `account`.id
+        INNER JOIN `book` ON `borrow`.book_id = `book`.id
+        INNER JOIN `book_detail` ON `book`.id = `book_detail`.id
+        WHERE `book`.id = $user_id AND `borrow`.return_date IS NULL";
+    
+        $result = $mysqli->query($query);
+        $mysqli->close();
+        return $result;
+    }
+?>

42d5cd9af689f16db5d0ac61b092db9f3f2e63ad
Linhchi162 <linhchi1622k4@gmail.com>
2024-04-08 00:47:29 +0700

    chưa xong

--- js/functions.js
+++ js/functions.js
@@ -32,3 +32,39 @@ function goToSignUp() {
 function showError() {
     document.getElementById("error_message").style.display = "block";
 }
+function goToAddBook() {
+    window.location.href = "addBook.html";
+}
+function goToGenre() {
+    window.location.href = "adminGenre.html";
+}
+function goToManual() {
+    document.querySelector(".manual-container").style.display = "block";
+    document.querySelector(".white_container").style.display = "none";
+}
+function goToSearch() {
+    document.querySelector(".manual-container").style.display = "none";
+    document.querySelector(".white_container").style.display = "block";
+}
+function goToModify() {
+    document.querySelector(".add-container").style.display = "none";
+}
+function goToAdd() {
+    document.querySelector(".add-container").style.display = "block";
+}
+function goToDetail() {
+    document.querySelector(".add-container").style.display = "none";
+}
+function goToEdit() {
+
+}
+function goToDelete() {
+
+}
+function goToMove() {
+    
+}
+
+
+
+

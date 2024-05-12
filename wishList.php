1e1f5748b3f367aec12f2e42d09df43d3caedf5a
cva2019 <caovananhnd2019@gmail.com>
2024-05-12 17:11:50 +0100

    Update wishList.php

--- wishList.php
+++ wishList.php
@@ -1,162 +1,34 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Wish list</title>
-        <link rel="stylesheet" href="css/wishList.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Shippori+Mincho&display=swap" rel="stylesheet">
-        <script src="functions.js"></script>
-        </head>
+<?php
+// Kết nối đến MongoDB
+include 'get_user.php';
+require 'C:\xampp\htdocs\LMS_project-main\LMS_project-main\vendor\autoload.php';
+$connectionString = 'mongodb+srv://caovananhnd2021:6ZtSq8I3XDCcX2z2@cluster0.nurixz1.mongodb.net';
+$manager = new MongoDB\Client($connectionString);
+$mongoDB = $manager->selectDatabase('library');
+$mongoCollection = $mongoDB->selectCollection('wishlist');
 
-    <body>
-        <div class="header">
-            <div class="start">logo</div>
-            <div class="middle">
-                <img class="find" src="img/icons8-search-48.png" width="18" height="18">
-                <input  class="enterSpace" type="text" placeholder="Search for name or author...">
-            </div>
-            <div class="end">
-              <div class="ava_menu">
-                <div >
-                    <img class="ava" src="img/81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" >
-                </div>
-                <div class="account_name">
-                    Account name
-                </div>
-                <img class="drop_down_menu" src="img/icons8-expand-arrow-64.png" width="25px" height="25px">
-              </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="menu">
-                    <button class="home" onclick="goToHome()">
-                        <img src="img\icons8-home-96.png" width="30" height="30">
-                        Home
-                    </button>
-                    <button class="bookshelf" onclick="goToBookshelf()">
-                        <img src="img/icons8-bookshelf-96.png" width="30" height="30">
-                        Bookshelf
-                    </button>
-                    <button class="wishlist" style=" background-color: #F8F7EC;">
-                        <img src="img/icons8-bookmark-96.png" width="30" height="30">
-                        Wishlist
-                    </button>
-                    <div>
-                        <button class="log_out_button" onclick="logOut()">
-                            <img src="img/icons8-log-out-96.png" width="30" height="30">
-                            Logout
-                        </button>
-                    </div>
-            </div>
-            <div class="main_board">
-                <div class="white_container">
-                    <div class="white_container2">
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                    <div class="book">
-                        <img class="book_cover" src="img\81nq+ewtkcL._AC_UF1000,1000_QL80_.jpg" width="120px" height="160px"> 
-                        <div class="name">name</div>
-                        <div class="author">author</div>
-                    </div>
-                </div>
+// Lấy thông tin từ MongoDB
+$user_id = $_SESSION['user_id'];
 
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+// Tạo một query để lấy dữ liệu từ MongoDB
+$query = [
+    'user_id' => $user_id
+];
+
+$cursor = $mongoCollection->find($query);
+
+$wishlistData = array();
+
+// Duyệt qua các tài liệu trả về
+foreach ($cursor as $document) {
+    $wishlistData[] = [
+        'id' => $document['book_id'],
+        'name' => $document['title'],
+        'author' => $document['author'],
+        'imageSrc' => $document['image']
+    ];
+}
+
+// Trả về kết quả dưới dạng JSON
+echo json_encode($wishlistData);
+?>
